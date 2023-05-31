3b44c1598f892583d639bc6534815de432591b5e
Valeria Foglino <valeriafoglino.78@gmail.com>
2023-05-31 14:22:12 -0300

    Añado archivos resta.js y multiplicación.js

---
+++ practico0/multiplicacion.js
@@ -0,0 +1,15 @@
+
+//Multiplicacion
+
+function multiplicacion(num1, num2, num3) {
+   console.log(num1*num2*num3) ;
+} 
+multiplicacion(2, 17, 6);
+
+//[Running] node "c:\Users\Usuario\Famaf-IntroJS\tempCodeRunnerFile.js"
+//204
+       //Otra Forma 
+        let n1=2;
+        let n2=17;
+        let n3=6;
+        console.log(n1*n2*n3);
---
+++ practico0/resta.js
@@ -0,0 +1,15 @@
+
+// Resta
+
+function resta(minuendo, sustraendo) {
+    console.log(minuendo - sustraendo) ;
+ } 
+ resta(10, 2);
+ 
+ //[Running] node "c:\Users\Usuario\Famaf-IntroJS\tempCodeRunnerFile.js"
+ //8
+
+  //Otra Forma 
+        let n1=10;
+        let n2=2;
+        console.log(n1-n2);
