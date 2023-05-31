1c24a27fa597cc0bab581891168029c3fee8946c
Valeria Foglino <valeriafoglino.78@gmail.com>
2023-05-31 14:27:51 -0300

    Práctico no finalizado

---
+++ practico0/areaRectangulo.js
@@ -0,0 +1,20 @@
+
+//Área Rectángulo
+
+function areaRectangulo(base, altura) {
+    console.log(base*altura) ;
+ } 
+ areaRectangulo(5, 3); // a)
+ areaRectangulo(12.5, 5.3); // b)
+ 
+ //[Running] node "c:\Users\Usuario\Famaf-IntroJS\tempCodeRunnerFile.js"
+ //15
+ //66.25
+                    //Otra Forma 
+                      let base=5;
+                      let altura=3;
+                      console.log(base*altura);
+
+                      let base2=12.5;
+                      let altura2=5.3;
+                      console.log(base2*altura2);
---
+++ practico0/promedio3.js
@@ -0,0 +1,22 @@
+//Promedio entre 3 números
+
+function promedio3(x, y, z) {
+    console.log((x+y+z)/3) ;
+ } 
+ //a. ¿Cuál es el promedio entre 5, 7 y 9?
+ promedio3(5, 7, 9);
+//b. ¿Cuál es el promedio entre -2 , -3, -8?
+ promedio3(-2, -3, -8);
+//c. ¿Cuál es el promedio entre -1.2, 5.3, -3.3?
+ promedio3(-1.2, 5.3, -3.3);
+ 
+ //[Running] node "c:\Users\Usuario\Famaf-IntroJS\tempCodeRunnerFile.js"
+// a)   7
+// b)  -4.333333333333333
+// c)   0.2666666666666666
+
+                            //Otra Forma 
+                            let n1=5;
+                            let n2=7;
+                            let n3=9;
+                            console.log((n1+n2+n3)/3);  //idem para cada caso reemplazando los 3 valores
