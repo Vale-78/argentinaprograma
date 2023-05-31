848f8789a9db23889894b12cc9fdc9c4fa91137b
Valeria Foglino <valeriafoglino.78@gmail.com>
2023-05-31 14:24:33 -0300

    “Agrego

---
+++ practico0/conversionGrados.js
@@ -0,0 +1,18 @@
+// Conversión a grdos Celsius
+
+function conversionGradosCelsius(fahrenheit) {
+    console.log((fahrenheit - 32)* 5/9);
+ } 
+ conversionGradosCelsius(50); // a)
+ conversionGradosCelsius(75); // b)
+ 
+// [Running] node "c:\Users\Usuario\Famaf-IntroJS\tempCodeRunnerFile.js"
+  // 10
+  // 23.88888888888889
+
+                    //Otra Forma 
+                    let fahrenheit = 50
+                    console.log((fahrenheit - 32)* 5/9);
+
+                    let fahrenheit2 = 75
+                    console.log((fahrenheit2 - 32)* 5/9);
