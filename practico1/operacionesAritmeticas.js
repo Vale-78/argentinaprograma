7e0175b622438045a77f3cdbc869a96287078a0b
Valeria Foglino <valeriafoglino.78@gmail.com>
2023-06-09 16:19:28 -0300

    Añado archivo operacionesAritmeticas

---
+++ practico1/operacionesAritmeticas.js
@@ -0,0 +1,29 @@
+let n1= 7;
+let n2= 4;
+let n3= 32;
+let n4= 28;
+// b. Suma las dos primeras variables y guarda el resultado en otra variable.
+let suma2primeros= (n1 + n2);
+console.log(suma2primeros);
+// c. Resta la cuarta variable de la tercera y almacena en variable.
+let terceraMenosCuarta= (n3 - n4);
+console.log(terceraMenosCuarta);
+// d. Multiplica los resultados. El producto debe ser 44. 
+let resultadoFinal= (suma2primeros * terceraMenosCuarta);
+console.log(resultadoFinal);
+// e. Escribe un cálculo que verifique si resultadoFinal es un número par.
+// Almacene el resultado en una variable llamada esPar.
+let esPar= (resultadoFinal%2==0);
+console.log(esPar);
+        
+// f. Imprima por consola un mensaje con el siguiente formato:
+console.log("Mis variables iniciales fueron: " + n1 + ", " + n2 + ", " + n3 + "y " + n4 + ". Y mi resultado final es par: " + esPar);
+
+
+// RESPUESTAS
+// [Running] node "c:\Users\Usuario\Famaf-IntroJS\practico1\tempCodeRunnerFile.js"
+// 11
+// 4
+// 44
+// true
+// Mis variables iniciales fueron: 7, 4, 32y 28. Y mi resultado final es par: true
