e0d5f386eb864de52fca3f46da5039d5a35545c0
Valeria Foglino <valeriafoglino.78@gmail.com>
2023-06-09 16:14:46 -0300

    Añado práctico1 con ejercicio 1

---
+++ practico1/miNombreCompleto.js
@@ -0,0 +1,11 @@
+let nombre;
+nombre = "Inés Valeria";
+let apellido = "Foglino";
+let edad = 45;
+console.log("Hola, " + nombre + " " + apellido + ". Tienes " + edad + " años.");
+
+//RESPUESTA
+
+// [Running] node "c:\Users\Usuario\Famaf-IntroJS\practico1\tempCodeRunnerFile.js"
+
+// Hola, Inés Valeria Foglino. Tienes 45 años.
