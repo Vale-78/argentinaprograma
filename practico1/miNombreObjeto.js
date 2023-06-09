26bedbc10699ac6402b2b9282a4ffd3febb8e4c9
Valeria Foglino <valeriafoglino.78@gmail.com>
2023-06-09 16:17:26 -0300

    Añado archivo miNombreObjeto

---
+++ practico1/miNombreObjeto.js
@@ -0,0 +1,11 @@
+const persona = {
+    nombre: "Inés Valeria",
+    apellido: "Foglino",
+    edad: 45,
+}
+console.log("Mi objeto persona es el siguiente: "+JSON.stringify(persona) );
+
+//RESPUESTA
+
+// [Running] node "c:\Users\Usuario\Famaf-IntroJS\practico1\tempCodeRunnerFile.js"
+// Mi objeto persona es el siguiente: {"nombre":"Inés Valeria","apellido":"Foglino","edad":45}
