c58c4d05f5ab32160c31694a0810f1890228f9e0
Valeria Foglino <valeriafoglino.78@gmail.com>
2023-06-16 18:18:03 -0300

    Modifiqué meses.js ya que había utilizado ciclos y no correspondía

--- practico2/meses.js
+++ practico2/meses.js
@@ -2,23 +2,33 @@
 // 12 representando los meses de un año; donde 1 es enero, 2 es febrero, 3 es
 // marzo, etc. Imprimir por consola la cantidad de días que tiene el mes bajo el siguiente formato:
 // "La cantidad de dias del mes de [mes] es [cantidadDeDias]"
-// Por ejemplo: si el número ingresado es 1, su programa imprimirá “La cantidad
-// de dias del mes de enero es 31”.
-// NOTAS:
-// ● Nota que el valor de [mes] no es el numero ingresado si no el almacenado en la estructura de datos elegida.
-// ● Para el alcance de este ejercicio, podemos pensar que febrero siempre
-// tiene 28 días.
 const readlineSync = require('readline-sync');
 const numeroMes = readlineSync.question('Ingresa un número entero del 1 al 12 representando los meses de un año: ');
 
-const meses = [{number:1, mes:"Enero", cantidadDeDias:31},{number:2, mes:"Febrero", cantidadDeDias:28}, {number:3, mes:"Marzo", cantidadDeDias:31}, {number:4, mes:"Abril", cantidadDeDias:30} ,{number:5, mes:"Mayo", cantidadDeDias:31} ,{number:6 , mes:"Junio", cantidadDeDias:30}, {number:7 , mes:"Julio", cantidadDeDias:31}, {number:8 , mes:"Agosto" ,cantidadDeDias:31}, {number: 9, mes:"Septiembre", cantidadDeDias:30}, {number:10 , mes:"Octubre" , cantidadDeDias:31}, {number:11 , mes:"Noviembre" , cantidadDeDias:30}, {number:12 ,mes:"Diciembre", cantidadDeDias:31}];
+const meses = [{mes:"Enero", cantidadDeDias:31},{mes:"Febrero", cantidadDeDias:28}, {mes:"Marzo", cantidadDeDias:31}, {mes:"Abril", cantidadDeDias:30} ,{mes:"Mayo", cantidadDeDias:31} ,{mes:"Junio", cantidadDeDias:30}, {mes:"Julio", cantidadDeDias:31}, {mes:"Agosto" ,cantidadDeDias:31}, {mes:"Septiembre", cantidadDeDias:30}, {mes:"Octubre" , cantidadDeDias:31}, {mes:"Noviembre" , cantidadDeDias:30}, {mes:"Diciembre", cantidadDeDias:31}];
 
-for(let i = 0; i < meses.length; i++){
-    if(numeroMes == meses[i].number){
-        let numero = numeroMes -1
-        console.log(`La cantidad de dias del mes de ${meses[numero].mes} es ${meses[numero].cantidadDeDias}`); 
-        break;   
-    }else{}
-}
-
- 
+  if(numeroMes == 1){
+    console.log(`La cantidad de dias del mes de ${meses[0].mes} es ${meses[0].cantidadDeDias}`); 
+ } else if(numeroMes == 2){
+    console.log(`La cantidad de dias del mes de ${meses[1].mes} es ${meses[1].cantidadDeDias}`); 
+}else if(numeroMes == 3){
+    console.log(`La cantidad de dias del mes de ${meses[2].mes} es ${meses[2].cantidadDeDias}`); 
+}else if(numeroMes == 4){
+    console.log(`La cantidad de dias del mes de ${meses[3].mes} es ${meses[3].cantidadDeDias}`); 
+}else if(numeroMes == 5){
+    console.log(`La cantidad de dias del mes de ${meses[4].mes} es ${meses[4].cantidadDeDias}`); 
+}else if(numeroMes == 6){
+    console.log(`La cantidad de dias del mes de ${meses[5].mes} es ${meses[5].cantidadDeDias}`); 
+}else if(numeroMes == 7){
+    console.log(`La cantidad de dias del mes de ${meses[6].mes} es ${meses[6].cantidadDeDias}`); 
+}else if(numeroMes == 8){
+    console.log(`La cantidad de dias del mes de ${meses[7].mes} es ${meses[7].cantidadDeDias}`); 
+}else if(numeroMes == 9){
+    console.log(`La cantidad de dias del mes de ${meses[8].mes} es ${meses[8].cantidadDeDias}`); 
+}else if(numeroMes == 10){
+    console.log(`La cantidad de dias del mes de ${meses[9].mes} es ${meses[9].cantidadDeDias}`); 
+}else if(numeroMes == 11){
+    console.log(`La cantidad de dias del mes de ${meses[10].mes} es ${meses[10].cantidadDeDias}`); 
+}else if(numeroMes == 12){
+    console.log(`La cantidad de dias del mes de ${meses[11].mes} es ${meses[11].cantidadDeDias}`); 
+};
